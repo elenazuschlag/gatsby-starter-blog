d0d55899d871d8e6058b01a89c9d95c12e29b26f
elenazuschlag <66139087+elenazuschlag@users.noreply.github.com>
2020-05-30 18:59:08 +0200

    bio6

--- src/components/bio.js
+++ src/components/bio.js
@@ -59,7 +59,7 @@ const Bio = () => {
       <p>
         Written by <strong>{author.name}</strong> {author.summary}
         {` `}
-        -        <a href={`https://twitter.com/${social.twitter}`}>
+        <a href={`https://twitter.com/${social.twitter}`}>
           You should follow her on GitHub
         </a>
       </p>
